4f709d6a9812ad9381837b00cdf23c0cd557aa17
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-23 21:44:34 +0200

    The initial commit.

---
+++ src/main/java/bm/app/macedonianarmyjpahibernate/MacedonianArmyJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package bm.app.macedonianarmyjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacedonianArmyJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacedonianArmyJpaHibernateApplication.class, args);
+    }
+
+}
